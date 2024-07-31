bee6e806451e8fcb2ac6c14e8409702a315b750b
Sergey Sokolov <se@sokoloff.su>
2024-07-31 15:48:23 +0300

    решение пока с ошибками

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,7 +1,9 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
+//import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -15,10 +17,12 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Audi", 1)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("BMW", 2)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Honda", 3)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Mercedes", 4)));
+
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,9 +30,25 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car model = " + user.getCar().getModel());
+         System.out.println("Car series = " + user.getCar().getSeries());
          System.out.println();
       }
+      /*
+      CarService carService = context.getBean(CarService.class);
+      carService.add(new Car("Audi", 1));
+      carService.add(new Car("BMW", 2));
+      carService.add(new Car("Honda", 3));
+      carService.add(new Car("Mercedes", 4));
 
-      context.close();
+      List<Car> cars = carService.listCars();
+      for (Car car : cars) {
+         System.out.println("Id = "+car.getId());
+         System.out.println("Model = "+car.getModel());
+         System.out.println("Series = "+car.getSeries());
+         System.out.println();
+      }
+*/
+         context.close();
    }
 }
